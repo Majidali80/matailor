dcf6ac9fd21412755b19fb0b9e26c988ecc10660
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-03-22 07:49:55 +0500

    Update cartContext.tsx

--- src/app/context/cartContext.tsx
+++ src/app/context/cartContext.tsx
@@ -1,5 +1,6 @@
+"use client"
 // app/context/cartContext.tsx
-import { createContext, useContext, useState } from "react";
+import { createContext, use, useContext, useState } from "react";
 import { CartItem } from "../utils/types";
 
 interface CartContextType {
